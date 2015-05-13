6a266e7dc34324be06678987dc0420d82b3558f4
Eric Mendes Dantas <ericdantas0@gmail.com>
2015-05-12 22:21:51 -0300

    .;.;.

--- client/dev/js/places/dao/places.dao.js
+++ client/dev/js/places/dao/places.dao.js
@@ -1,1 +1,11 @@
+(function(angular)
+{
+  "use strict";
 
+  angular
+    .module('randomLunch')
+    .factory('PlacesDAO', ['$q', 'PlacesResource', function($q, PlacesResource)
+    {
+
+    }]);
+}(window.angular));
